c8f03f86a6032de49bcae6f74e10e7f3ccc832e1
Mrec2 <marioteleco@outlook.com>
2025-02-28 17:00:32 +0100

    Axios functionality was added inside util folder. It was created a button in order to check the connection with the backend and it works.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
+"use client"; 
 
+import { useState } from "react";
+import api from "@/utils/initAxios";
 
 export default function Home() {
+  const [data, setData] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get("/api/message"); 
+      console.log(response.data);
+      setData(response.data); 
+    } catch (error) {
+      console.error("Error al obtener los datos:", error);
+      setData("Error en la petición.");
+    }
+    setLoading(false);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <h1 className="text-4xl font-bold text-white">BIENVENIDO A PETGUARDIAN</h1>
-      <p className="text-lg  mt-4">Conectando refugios y rescatistas con padrinos</p>
+    <div className="h-screen flex flex-col items-center justify-center text-center bg-black text-white">
+      <h1 className="text-4xl font-bold text-yellow-400 mb-4">🐾 Bienvenido a PetGuardian 🐱</h1>
+      <button
+        onClick={fetchData}
+        className="mt-6 px-6 py-2 bg-yellow-400 text-black font-bold rounded-lg shadow hover:bg-yellow-500 transition"
+      >
+        Obtener Datos
+      </button>
+      {loading && <p className="mt-4">Cargando...</p>}
+      {data && <p className="mt-4 text-lg">{data}</p>}
     </div>
   );
 }
-
